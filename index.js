1366ee9f7ea8fc8f3db89309d5ce5c35dae78a1f
DeathTraq <oktayesmaokyay@hotmail.com>
2022-01-17 18:10:07 +0100

    initialize github repository

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { prefix, token } = require("./config.json");
 const ytdl = require("ytdl-core");
 require("dotenv").config();  
 
+
 const client = new Discord.Client();
 
 const queue = new Map();
